5749a4fd60f098f502f1f0f21fdb165f3ea5c2de
ncleexx-beep <ncleexx@gmail.com>
2026-04-29 21:20:34 +0800

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+}
